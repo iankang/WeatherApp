e50381d866b02ce94db74ddb0acda7870102277e
Ian kangethe <iankangethe@Ians-MacBook-Pro.local>
2023-02-09 04:26:30 +0300

    added location permissions

--- app/src/main/java/com/dvt/weatherapp/MainActivity.kt
+++ app/src/main/java/com/dvt/weatherapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dvt.weatherapp
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,12 +11,16 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -23,15 +28,19 @@ import androidx.navigation.compose.rememberNavController
 import com.dvt.weatherapp.navigation.NavDrawerItem
 import com.dvt.weatherapp.screens.*
 import com.dvt.weatherapp.ui.theme.WeatherAppTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             WeatherAppTheme {
                 val navController = rememberNavController()
+                MultiplePermissions()
                 MainScaffold(navController)
             }
         }
@@ -121,5 +130,94 @@ fun MainScaffold(
 fun DefaultPreview() {
     WeatherAppTheme {
         MainScaffold(null)
+    }
+}
+
+
+@ExperimentalPermissionsApi
+@Composable
+fun MultiplePermissions() {
+    val permissionStates = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+        )
+    )
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    DisposableEffect(key1 = lifecycleOwner, effect = {
+        val observer = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_START -> {
+                    permissionStates.launchMultiplePermissionRequest()
+                }
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    })
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    )
+    {
+        permissionStates.permissions.forEach { it ->
+            when (it.permission) {
+                Manifest.permission.ACCESS_FINE_LOCATION -> {
+                    when {
+                        it.hasPermission -> {
+                            /* Permission has been granted by the user.
+                               You can use this permission to now acquire the location of the device.
+                               You can perform some other tasks here.
+                            */
+                            Text(text = "Read Ext Storage permission has been granted")
+                        }
+                        it.shouldShowRationale -> {
+                            /*Happens if a user denies the permission two times
+
+                             */
+                            Text(text = "Read Ext Storage permission is needed")
+                        }
+                        !it.hasPermission && !it.shouldShowRationale -> {
+                            /* If the permission is denied and the should not show rationale
+                                You can only allow the permission manually through app settings
+                             */
+                            Text(text = "Navigate to settings and enable the Storage permission")
+
+                        }
+                    }
+                }
+                Manifest.permission.ACCESS_COARSE_LOCATION -> {
+                    when {
+                        it.hasPermission -> {
+                            /* Permission has been granted by the user.
+                               You can use this permission to now acquire the location of the device.
+                               You can perform some other tasks here.
+                            */
+                            Text(text = "Location permission has been granted")
+                        }
+                        it.shouldShowRationale -> {
+                            /*Happens if a user denies the permission two times
+
+                             */
+                            Text(text = "Location permission is needed")
+
+                        }
+                        !it.hasPermission && !it.shouldShowRationale -> {
+                            /* If the permission is denied and the should not show rationale
+                                You can only allow the permission manually through app settings
+                             */
+                            Text(text = "Navigate to settings and enable the Location permission")
+
+                        }
+                    }
+                }
+
+            }
+        }
     }
 }
